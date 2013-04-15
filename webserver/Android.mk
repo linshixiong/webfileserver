a54e5aeaccab246125449c4324a3c649f983e9de
linshixiong <linsx123@qq.com>
2013-04-15 20:04:39 +0800

    使用zxing库代替google chart API生成二维码

--- webserver/Android.mk
+++ webserver/Android.mk
@@ -3,8 +3,16 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_STATIC_JAVA_LIBRARIES := zxing
+
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_PACKAGE_NAME := WebServer
 
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := zxing:libs/zxing.jar
+
+include $(BUILD_MULTI_PREBUILT)
--- webserver/src/com/linsx/webserver/Utils.java
+++ webserver/src/com/linsx/webserver/Utils.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Hashtable;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -19,6 +20,12 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.util.InetAddressUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -77,6 +84,50 @@ public class Utils {
 		return network.isAvailable();
 	}
 
+	// 生成QR图
+    public static Bitmap createQRImage(String text,int width,int height) {
+        try {
+            // 需要引入core包
+            QRCodeWriter writer = new QRCodeWriter();
+ 
+            if (text == null || "".equals(text) || text.length() < 1) {
+                return null;
+            }
+
+            // 把输入的文本转为二维码
+            BitMatrix martix = writer.encode(text, BarcodeFormat.QR_CODE,
+            		width, height);
+
+            System.out.println("w:" + martix.getWidth() + "h:"
+                    + martix.getHeight());
+
+            Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+            BitMatrix bitMatrix = new QRCodeWriter().encode(text,
+                    BarcodeFormat.QR_CODE, width, height, hints);
+            int[] pixels = new int[width * height];
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    if (bitMatrix.get(x, y)) {
+                        pixels[y * width + x] = 0xff000000;
+                    } else {
+                        pixels[y * width + x] = 0xffffffff;
+                    }
+
+                }
+            }
+
+            Bitmap bitmap = Bitmap.createBitmap(width, height,
+                    Bitmap.Config.ARGB_8888);
+
+            bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+            return bitmap;
+        } catch (WriterException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+	
 	public static Bitmap Create2DCode(String str) {
 		// httpGet连接对象
 		HttpGet httpRequest = new HttpGet(String.format(QRCODE_API,str));
